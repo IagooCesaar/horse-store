3d56ee9ff4c73748e959cf508f214120d3ba8450
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-06-05 15:39:52 -0300

    Implementação teste obter histórico movimento e saldo atual item

--- server/src/test/controller/Loja.Controller.Estoque.Test.pas
+++ server/src/test/controller/Loja.Controller.Estoque.Test.pas
@@ -43,7 +43,8 @@ uses
 
   Loja.Controller.Api.Test,
   Loja.Model.Dto.Req.Itens.CriarItem,
-  Loja.Model.Dto.Req.Estoque.AcertoEstoque;
+  Loja.Model.Dto.Req.Estoque.AcertoEstoque,
+  Loja.Model.Dto.Resp.Estoque.SaldoItem;
 
 { TLojaControllerEstoqueTest }
 
@@ -101,16 +102,77 @@ begin
   Assert.AreEqual(201, LResponse.StatusCode);
 
   LAcerto.Free;
+  LItem.Free;
 end;
 
 procedure TLojaControllerEstoqueTest.Test_ObterHistoricoMovimento;
 begin
+  var LItem := CriarItem;
+  var LAcerto := TLojaModelDtoReqEstoqueAcertoEstoque.Create;
+  LAcerto.CodItem := LItem.CodItem;
+  LACerto.QtdSaldoReal := 1;
+  LAcerto.DscMot := 'Implantação Teste';
 
+  var LResponseAcerto := TRequest.New
+      .BasicAuthentication(FUsarname, FPassword)
+      .BaseURL(FBaseURL)
+      .Resource('/estoque/{cod_item}/acerto-de-estoque')
+      .AddUrlSegment('cod_item', LItem.CodItem.ToString)
+      .AddBody(TJson.ObjectToClearJsonString(LAcerto))
+      .Post();
+
+  Assert.AreEqual(201, LResponseAcerto.StatusCode);
+
+  var LResponse:= TRequest.New
+      .BasicAuthentication(FUsarname, FPassword)
+      .BaseURL(FBaseURL)
+      .Resource('/estoque/{cod_item}/historico-movimento')
+      .AddUrlSegment('cod_item', LItem.CodItem.ToString)
+      .AddParam('dat_ini', FormatDateTime('yyyy-mm-dd', Now))
+      .AddParam('dat_fim', FormatDateTime('yyyy-mm-dd', Now))
+      .Get();
+
+  Assert.AreEqual(200, LResponse.StatusCode);
+
+  LAcerto.Free;
+  LItem.Free;
 end;
 
 procedure TLojaControllerEstoqueTest.Test_ObterSaldoAtual;
 begin
+  var LItem := CriarItem;
+  var LAcerto := TLojaModelDtoReqEstoqueAcertoEstoque.Create;
+  LAcerto.CodItem := LItem.CodItem;
+  LACerto.QtdSaldoReal := 1;
+  LAcerto.DscMot := 'Implantação Teste';
 
+  var LResponseAcerto := TRequest.New
+      .BasicAuthentication(FUsarname, FPassword)
+      .BaseURL(FBaseURL)
+      .Resource('/estoque/{cod_item}/acerto-de-estoque')
+      .AddUrlSegment('cod_item', LItem.CodItem.ToString)
+      .AddBody(TJson.ObjectToClearJsonString(LAcerto))
+      .Post();
+
+  Assert.AreEqual(201, LResponseAcerto.StatusCode);
+
+  var LResponse:= TRequest.New
+      .BasicAuthentication(FUsarname, FPassword)
+      .BaseURL(FBaseURL)
+      .Resource('/estoque/{cod_item}/saldo-atual')
+      .AddUrlSegment('cod_item', LItem.CodItem.ToString)
+      .Get();
+
+  Assert.AreEqual(200, LResponse.StatusCode);
+
+  var LSaldo := TJson.ClearJsonAndConvertToObject<TLojaModelDtoRespEstoqueSaldoItem>
+    (LResponse.Content);
+
+  Assert.AreEqual(1, LSaldo.QtdSaldoAtu);
+
+  LAcerto.Free;
+  LItem.Free;
+  LSaldo.Free;
 end;
 
 initialization
