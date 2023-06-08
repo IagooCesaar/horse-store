096c1001b5f08a25dfed7bf371d10eb799badf0f
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-06-08 11:22:00 -0300

    ignorando propriedade CodItem no dto/Loja.Model.Dto.Req.Estoque.AcertoEstoque.pas

--- server/src/model/dto/Loja.Model.Dto.Req.Estoque.AcertoEstoque.pas
+++ server/src/model/dto/Loja.Model.Dto.Req.Estoque.AcertoEstoque.pas
@@ -3,7 +3,8 @@ unit Loja.Model.Dto.Req.Estoque.AcertoEstoque;
 interface
 
 uses
-  System.Classes;
+  System.Classes,
+  GBSwagger.Model.Attributes;
 
 type
   TLojaModelDtoReqEstoqueAcertoEstoque = class
@@ -12,7 +13,9 @@ type
     FQtdSaldoReal: Integer;
     FDscMot: string;
   public
+    [SwagIgnore]
     property CodItem: integer read FCodItem write FCodItem;
+
     property QtdSaldoReal: Integer read FQtdSaldoReal write FQtdSaldoReal;
     property DscMot: string read FDscMot write FDscMot;
   end;
