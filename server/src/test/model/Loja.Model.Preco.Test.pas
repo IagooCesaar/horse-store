ba228c79b8280aacde7b6bbfbcacbbd34a90128f
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-06-17 12:35:37 -0300

    Implementação teste de criação de preço de venda para item

--- server/src/test/model/Loja.Model.Preco.Test.pas
+++ server/src/test/model/Loja.Model.Preco.Test.pas
@@ -3,6 +3,7 @@ unit Loja.Model.Preco.Test;
 interface
 
 uses
+  System.SysUtils,
   DUnitX.TestFramework,
   Loja.Model.Entity.Itens.Item,
   Loja.Model.Dto.Req.Itens.CriarItem;
@@ -11,28 +12,34 @@ type
   [TestFixture]
   TLojaModelPrecoTest = class
   private
-    function CriarItem: TLojaModelEntityItensItem;
+    function CriarItem(ANome, ACodBarr: String): TLojaModelEntityItensItem;
   public
     [SetupFixture]
     procedure SetupFixture;
 
     [TearDownFixture]
     procedure TearDownFixture;
+
+    [Test]
+    procedure Test_CriarPrecoVenda;
   end;
 
 implementation
 
 uses
-  Loja.Model.Dao.Factory;
+  Loja.Model.Factory,
+  Loja.Model.Dao.Factory,
+  Loja.Model.Dto.Req.Preco.CriarPrecoVenda;
 
 { TLojaModelPrecoTest }
 
-function TLojaModelPrecoTest.CriarItem: TLojaModelEntityItensItem;
+function TLojaModelPrecoTest.CriarItem(ANome, ACodBarr: String): TLojaModelEntityItensItem;
 var LDTONovoItem: TLojaModelDtoReqItensCriarItem;
 begin
   LDTONovoItem := TLojaModelDtoReqItensCriarItem.Create;
   try
-    LDTONovoItem.NomItem := 'TLojaModelPrecoTest.CriarItem';
+    LDTONovoItem.NomItem := ANome;
+    LDTONovoItem.NumCodBarr := ACodBarr;
     Result := TLojaModelDaoFactory.New.Itens.Item.CriarItem(LDTONovoItem);
   finally
     LDTONovoItem.Free;
@@ -47,6 +54,26 @@ end;
 procedure TLojaModelPrecoTest.TearDownFixture;
 begin
   TLojaModelDaoFactory.InMemory := False;
+end;
+
+procedure TLojaModelPrecoTest.Test_CriarPrecoVenda;
+begin
+  var LItemCriado := CriarItem('TLojaModelPrecoTest.Test_CriarPrecoVenda', '');
+  var LDto := TLojaModelDtoReqPrecoCriarPrecoVenda.Create;
+  LDto.CodItem := LItemCriado.CodItem;
+  LDto.DatIni := Now;
+  LDto.VrVnda := 1.99;
+
+  var LPreco := TLojaModelFactory.New
+    .Preco
+    .CriarPrecoVendaItem(LDto);
+
+  Assert.AreEqual(LDto.VrVnda, LPreco.VrVnda);
+  Assert.AreEqual(LDto.DatIni, LPreco.DatIni);
+
+  LPreco.Free;
+  LDto.Free;
+  LItemCriado.Free;
 end;
 
 initialization
