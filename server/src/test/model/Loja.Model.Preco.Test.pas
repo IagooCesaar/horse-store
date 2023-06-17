529d219c70f5519c0a3ac6c187005a5e88fee479
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2023-06-17 12:50:44 -0300

    Testes para criação de preço de venda

--- server/src/test/model/Loja.Model.Preco.Test.pas
+++ server/src/test/model/Loja.Model.Preco.Test.pas
@@ -22,11 +22,23 @@ type
 
     [Test]
     procedure Test_CriarPrecoVenda;
+
+    [Test]
+    procedure Test_NaoCriarPrecoVenda_ItemInexiste;
+
+    [Test]
+    procedure Test_NaoCriarPrecoVenda_ValorNegativo;
+
+    [Test]
+    procedure Test_NaoCriarPrecoVenda_PrecoJaExistente;
   end;
 
 implementation
 
 uses
+  Horse,
+  Horse.Exception,
+
   Loja.Model.Factory,
   Loja.Model.Dao.Factory,
   Loja.Model.Dto.Req.Preco.CriarPrecoVenda;
@@ -72,6 +84,79 @@ begin
   Assert.AreEqual(LDto.DatIni, LPreco.DatIni);
 
   LPreco.Free;
+  LDto.Free;
+  LItemCriado.Free;
+end;
+
+procedure TLojaModelPrecoTest.Test_NaoCriarPrecoVenda_ItemInexiste;
+begin
+  var LDto := TLojaModelDtoReqPrecoCriarPrecoVenda.Create;
+  LDto.CodItem := -1;
+  LDto.DatIni := Now;
+  LDto.VrVnda := 1.99;
+
+  Assert.WillRaiseWithMessageRegex(
+    procedure begin
+      TLojaModelFactory.New
+        .Preco
+        .CriarPrecoVendaItem(LDto);
+    end,
+    EHorseException,
+    'Não foi possível encontrar o item pelo código informado'
+  );
+
+  LDto.Free;
+end;
+
+procedure TLojaModelPrecoTest.Test_NaoCriarPrecoVenda_PrecoJaExistente;
+begin
+  var LItemCriado := CriarItem('TLojaModelPrecoTest.Test_NaoCriarPrecoVenda_PrecoJaExistente', '');
+
+  var LDto := TLojaModelDtoReqPrecoCriarPrecoVenda.Create;
+  LDto.CodItem := LItemCriado.CodItem;
+  LDto.DatIni := Now;
+  LDto.VrVnda := 1.99;
+
+  var LPreco := TLojaModelFactory.New
+    .Preco
+    .CriarPrecoVendaItem(LDto);
+
+  LDto.VrVnda := 2.01;
+
+  Assert.WillRaiseWithMessageRegex(
+    procedure begin
+      TLojaModelFactory.New
+        .Preco
+        .CriarPrecoVendaItem(LDto);
+    end,
+    EHorseException,
+    'Já existe um preço configurado para inicar na data informada'
+  );
+
+  LDto.Free;
+  LPreco.Free;
+  LItemCriado.Free;
+end;
+
+procedure TLojaModelPrecoTest.Test_NaoCriarPrecoVenda_ValorNegativo;
+begin
+  var LItemCriado := CriarItem('TLojaModelPrecoTest.Test_NaoCriarPrecoVenda_ValorNegativo', '');
+
+  var LDto := TLojaModelDtoReqPrecoCriarPrecoVenda.Create;
+  LDto.CodItem := LItemCriado.CodItem;
+  LDto.DatIni := Now;
+  LDto.VrVnda := -0.01;
+
+  Assert.WillRaiseWithMessageRegex(
+    procedure begin
+      TLojaModelFactory.New
+        .Preco
+        .CriarPrecoVendaItem(LDto);
+    end,
+    EHorseException,
+    'O valor de venda deve ser maior que zero'
+  );
+
   LDto.Free;
   LItemCriado.Free;
 end;
