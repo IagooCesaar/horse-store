6ac2ffeb0140e5b0087fc2a234134678a34034fa
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2023-09-13 07:38:58 -0300

    #5 atualização Entity Itens

--- server/src/model/entity/itens/Loja.Model.Entity.Itens.Item.pas
+++ server/src/model/entity/itens/Loja.Model.Entity.Itens.Item.pas
@@ -12,10 +12,15 @@ type
     FCodItem: Integer;
     FNomItem: String;
     FNumCodBarr: string;
+    FFlgPermSaldNeg: string;
+    FFlgTabPreco: string;
   public
     property CodItem: Integer read FCodItem write FCodItem;
     property NomItem: String read FNomItem write FNomItem;
     property NumCodBarr: string read FNumCodBarr write FNumCodBarr;
+    property FlgPermSaldNeg: string read FFlgPermSaldNeg write FFlgPermSaldNeg;
+    property FlgTabPreco: string read FFlgTabPreco write FFlgTabPreco;
+
   end;
 
   TLojaModelEntityItensItemLista = TObjectList<TLojaModelEntityItensItem>;
