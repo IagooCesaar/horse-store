0689ab32dbd96dbacc435d6b0f9a156f3080c172
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2023-09-13 07:40:56 -0300

    #5 atualização DTO Criar Item

--- server/src/model/dto/Loja.Model.Dto.Req.Itens.CriarItem.pas
+++ server/src/model/dto/Loja.Model.Dto.Req.Itens.CriarItem.pas
@@ -14,12 +14,17 @@ type
     FNomItem: String;
     FNumCodBarr: string;
     FCodItem: Integer;
+    FFlgPermSaldNeg: string;
+    FFlgTabPreco: string;
+
   public
     [SwagIgnore]
     property CodItem: Integer read FCodItem write FCodItem;
 
     property NomItem: String read FNomItem write FNomItem;
     property NumCodBarr: string read FNumCodBarr write FNumCodBarr;
+    property FlgPermSaldNeg: string read FFlgPermSaldNeg write FFlgPermSaldNeg;
+    property FlgTabPreco: string read FFlgTabPreco write FFlgTabPreco;
   end;
 
 implementation
