e0705353d06310546fbe71d74d1d6d820a389279
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2023-09-13 07:32:44 -0300

    Renomeado campo de FLG_CTRL_ESTQ (Controla Estoque) para FLG_PERM_SALD_NEG (Permite Saldo Negativo). O item terá movimento de estoque, mesmo que gere saldo negativo

--- server/database/20230913_073000_CamposItemGenerico.sql
+++ server/database/20230913_073000_CamposItemGenerico.sql
@@ -2,12 +2,12 @@ CREATE DOMAIN FLAG AS CHAR(1) CHECK (VALUE IN ('S','N'));
 
 COMMIT;
 
-ALTER TABLE ITEM ADD FLG_CTRL_ESTQ FLAG DEFAULT 'S' ;
+ALTER TABLE ITEM ADD FLG_PERM_SALD_NEG FLAG DEFAULT 'S' ;
 ALTER TABLE ITEM ADD FLG_TAB_PRECO FLAG DEFAULT 'S' ;
 
 COMMIT;
 
-update item set flg_ctrl_estq = 'S', flg_tab_preco = 'S' where 1=1
+update item set FLG_PERM_SALD_NEG = 'S', flg_tab_preco = 'S' where 1=1
 
 COMMIT;
 
